3d0bb0952e5964f6728709d67c64156f3cd1ad41
franky <franky@bitbox.be>
2026-03-28 10:02:05 +0100

    Spec 003 ads: task 2

---
+++ ollander-front/app/components/AdInArticle.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import AdBlock from "~/components/AdBlock.vue";
+</script>
+
+<template>
+  <AdBlock ad-slot="8874168294" ad-format="fluid" />
+</template>
---
+++ ollander-front/app/components/AdSidebar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import AdBlock from "~/components/AdBlock.vue";
+</script>
+
+<template>
+  <AdBlock ad-slot="5993821684" ad-format="auto" />
+</template>
